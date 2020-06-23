dae605139a4bf6e45f10791246719d8cf7bcd914
Graham Gilbert <graham.gilbert@airbnb.com>
2020-06-23 12:57:11 -0700

    Fixings

--- lib/puppet/functions/send_mdm_profile.rb
+++ lib/puppet/functions/send_mdm_profile.rb
@@ -1,23 +1,24 @@
 # I wish this was Go. Or Python. Or anything else. But here we are.
 
-require 'net/http'
-require 'uri'
-require 'json'
-require 'base64'
+require "net/http"
+require "net/https"
+require "uri"
+require "json"
+require "base64"
 
-require 'puppet/util/plist' if Puppet.features.cfpropertylist?
+require "puppet/util/plist" if Puppet.features.cfpropertylist?
 
 Puppet::Functions.create_function(:send_mdm_profile) do
-
-  def send_mdm_profile(mobileconfig, udid, ensure_profile, mdmdirector_password, mdmdirector_username, mdmdirector_host, mdmdirector_path)
-
-
+  def send_mdm_profile(mobileconfig, udid, ensure_profile, mdmdirector_username, mdmdirector_password, mdmdirector_host, mdmdirector_path = "/profile")
     enc = Base64.encode64(mobileconfig)
+    unless mdmdirector_path.start_with?("/")
+      mdmdirector_path = "/" + mdmdirector_path
+    end
+    uri = URI.parse(mdmdirector_host + mdmdirector_path)
+    http = Net::HTTP.new(uri.host, uri.port)
 
-    uri = URI.parse(mdmdirector_host)
-    uri += mdmdirector_path
     if ensure_profile == "absent"
-      request = Net::HTTP::Delete.new(uri.path)
+      request = Net::HTTP::Delete.new(uri.request_uri)
       # also need to parse out the payload id from the plist
       plist = Puppet::Util::Plist.parse_plist(mobileconfig)
 
@@ -25,41 +26,31 @@ Puppet::Functions.create_function(:send_mdm_profile) do
         "udids" => [udid],
         "profiles" => [{
           "payload_identifier" => plist["PayloadIdentifier"],
-          "uuid" => plist["PayloadUUID"]
+          "uuid" => plist["PayloadUUID"],
         }],
-        "metadata" => true
+        "metadata" => true,
       })
     else
-      request = Net::HTTP::Post.new(uri)
-      request.body = JSON.dump({
+      request = Net::HTTP::Post.new(uri.request_uri)
+      data = {
         "udids" => [udid],
         "profiles" => [enc],
         "metadata" => true,
-        "push_now" => true
-      })
+        "push_now" => true,
+      }
+      request.body = data.to_json
+    end
+
+    if uri.scheme == "https"
+      http.use_ssl = true
+    else
+      http.use_ssl = false
     end
     request.basic_auth(mdmdirector_username, mdmdirector_password)
 
+    response = http.request(request)
 
-    req_options = {
-      use_ssl: uri.scheme == "https",
-    }
-
-    response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
-      http.request(request)
-    end
-
-    # device = JSON.parse(response.body)
-    # if device["profile_metadata"][0]["status"] == "pushed" or device["profile_metadata"][0]["status"] == "changed"
-    #   state = "changed"
-    # else
-    #   state = device["profile_metadata"][0]["status"]
-    # end
-
-    # for profilelist
-    # output = {
-    #   "state" => state
-    # }
+    Puppet.debug(response)
     JSON.parse(response.body)
   end
 end
